d2b098bce9b67714cf02f8ac71b2562f1d51991d
Micha Kops <micha.kops@gmail.com>
2019-02-28 20:04:09 +0100

    Example with additional metadata added

--- src/main/java/com/hascode/tutorial/App.java
+++ src/main/java/com/hascode/tutorial/App.java
@@ -1,9 +1,13 @@
 package com.hascode.tutorial;
 
 import io.ap4k.kubernetes.annotation.KubernetesApplication;
+import io.ap4k.kubernetes.annotation.Mount;
+import io.ap4k.kubernetes.annotation.Port;
 import java.time.Instant;
 
-@KubernetesApplication
+@KubernetesApplication(ports = @Port(name = "http", containerPort = 8080),
+    mounts = @Mount(name = "mysql-volume", path = "/var/lib/mysql")
+)
 public class App {
 
   public static void main(String[] args) {
